87d132d43275013cbd18815493543209043fcb64
valiquette <66563472+valiquette@users.noreply.github.com>
2022-02-10 08:02:53 -0700

    V1.0.0

---
+++ index.js
@@ -0,0 +1,12 @@
+const PlatformOrbit = require('./wallboxplatform')
+
+module.exports = (homebridge) => {
+  PlatformAccessory = homebridge.platformAccessory
+  Service = homebridge.hap.Service
+  Characteristic = homebridge.hap.Characteristic
+  UUIDGen = homebridge.hap.uuid
+  PluginName = 'homebridge-my-wallbox'
+  PlatformName = 'wallbox'
+  
+  homebridge.registerPlatform(PluginName, PlatformName, PlatformOrbit, true)
+}
