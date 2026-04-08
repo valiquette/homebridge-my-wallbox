da3105cd2d7a15760c9dc98d4b99588336f7fd1d
valiquette <66563472+valiquette@users.noreply.github.com>
2026-04-07 19:17:16 -0600

    v1.1.27
    API change

--- src/devices/sensor.ts
+++ src/devices/sensor.ts
@@ -12,12 +12,11 @@ export default class sensor {
 	createSensorService(device: any, type: string): Service {
 		this.platform.log.info('Adding sensor for %s charger ', device.name);
 		this.platform.log.debug('create new sensor');
-		let humiditySensor = new this.platform.Service.HumiditySensor(type, device.id);
 		let stateOfCharge = 0;
 		if (device.stateOfCharge) {
 			stateOfCharge = device.stateOfCharge;
 		}
-		humiditySensor = new this.platform.Service.HumiditySensor(type, device.id);
+		const humiditySensor = new this.platform.Service.HumiditySensor(type, device.id);
 		humiditySensor.setCharacteristic(this.platform.Characteristic.Name, device.name + ' ' + type).setCharacteristic(this.platform.Characteristic.CurrentRelativeHumidity, stateOfCharge);
 		return humiditySensor;
 	}
